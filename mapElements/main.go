1cd1fec7a0b06c98748dc069dbb7a4316e3e2a22
Urian <uchang@sightmachine.com>
2017-07-12 19:21:55 -0700

    Finished adding example about maps and periodic elements

--- mapElements/main.go
+++ mapElements/main.go
@@ -15,34 +15,67 @@ func main() {
   // elements["O"] = "Oxygen"
   // elements["F"] = "Fluorine"
   // elements["Ne"] = "Neon"
+  //
+  // fmt.Println(elements["Li"])
+  //
+  // //: Returns zero value for key that doesn't exist
+  // fmt.Println(elements["Um"])
+  //
+  // //: Check if element exists -> returns value, Boolean
+  // name, ok := elements["Un"]
+  // fmt.Println(name, ok)
+  //
+  // //: First try to get value from map, then if successful, run code block
+  // if name, ok := elements["Um"]; ok {
+  //   fmt.Println(name, ok)
+  // }
 
   //: Faster ways of creating maps
-  elements := map[string]string{
-    "H": "Hydrogen",
-    "He": "Helium",
-    "Li": "Lithium",
-    "Be": "Beryllium",
-    "B": "Boron",
-    "C": "Carbon",
-    "N": "Nitrogen",
-    "O": "Oxygen",
-    "F": "Fluorine",
-    "Ne": "Neon",
+  elements := map[string]map[string]string{
+    "H": map[string]string{
+      "name": "Hydrogen",
+      "state": "gas",
+    },
+    "He": map[string]string{
+      "name": "Helium",
+      "state": "gas",
+    },
+    "Li": map[string]string{
+      "name": "Lithium",
+      "state": "solid",
+    },
+    "Be": map[string]string{
+      "name": "Beryllium",
+      "state": "solid",
+    },
+    "B": map[string]string{
+      "name": "Boron",
+      "state": "solid",
+    },
+    "C": map[string]string{
+      "name": "Carbon",
+      "state": "solid",
+    },
+    "N": map[string]string{
+      "name": "Nitrogen",
+      "state": "gas",
+    },
+    "O": map[string]string{
+      "name": "Oxygen",
+      "state": "gas",
+    },
+    "F": map[string]string{
+      "name": "Fluorine",
+      "state": "gas",
+    },
+    "Ne": map[string]string{
+      "name": "Neon",
+      "state": "gas",
+    },
   }
 
-  fmt.Println(elements["Li"])
-
-  //: Returns zero value for key that doesn't exist
-  fmt.Println(elements["Um"])
-
-  //: Check if element exists -> returns value, Boolean
-  name, ok := elements["Un"]
-  fmt.Println(name, ok)
-
-  //: First try to get value from map, then if successful, run code block
-  if name, ok := elements["Um"]; ok {
-    fmt.Println(name, ok)
+  if el, ok := elements["Li"]; ok {
+    fmt.Println(el["name"], el["state"])
   }
-
 
 }
