916f4226729138b39ef8b1e316481e2c962b8c0a
Urian <uchang@sightmachine.com>
2017-07-12 18:59:58 -0700

    Added simple example of map

--- maps/main.go
+++ maps/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-  var x map[string]int
+  x := make(map[string]int)
+
   x["key"] = 10
-  fmt.Println(x)
+  x["key2"] = 11
+
+  fmt.Println(x["key"])   //: maps are not sequential
+  fmt.Println(len(x))   //: Length of map can change
+
+  //: Delete: (map, key)
+  delete(x, "key")
 }
