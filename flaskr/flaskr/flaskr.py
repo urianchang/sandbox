0181e0e4e84f70d42250b6889e510ad8f03baf09
Urian <uchang@sightmachine.com>
2017-08-10 19:02:45 -0700

    Added code to create/initialize the db

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -28,6 +28,19 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+# Initializes database to the app
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+# Registers command with flask script
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print "Initialized the database."
+
 # Opens new db connection if there is none yet for current app context
 def get_db():
     if not hasattr(g, 'sqlite_db'):
