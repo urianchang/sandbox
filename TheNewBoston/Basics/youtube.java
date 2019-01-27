58729b57d5691524e9605a87fc24b51d5f94a78b
Urian <uchang@sightmachine.com>
2019-01-26 21:25:12 -0800

    Created basic addition calculator

--- TheNewBoston/Basics/youtube.java
+++ TheNewBoston/Basics/youtube.java
@@ -1,5 +1,13 @@
-class apples{
-    public static void main(String args[]){
-        System.out.println("Hello youtube!");
+import java.util.Scanner;
+
+class additionCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double first, second;
+        System.out.println("Enter first number: ");
+        first = scan.nextDouble();
+        System.out.println("Enter second number ");
+        second = scan.nextDouble();
+        System.out.println(first + second);
     }
 }
