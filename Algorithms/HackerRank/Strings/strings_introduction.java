76b4317898f27f4252038275142305a862e1c170
Urian <uchang@sightmachine.com>
2019-02-13 14:53:51 -0800

    Completed Strings Introduction exercise

---
+++ Algorithms/HackerRank/Strings/strings_introduction.java
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/java-strings-introduction/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+
+        // Sum lengths of A and B
+        int len = A.length() + B.length();
+        System.out.println(len);
+
+        // Determine if A is lexicographically larger than B
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        // Capitalize the first letter in A and B -> print on same line separated by space
+        String capA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capA + " " + capB);
+    }
+}
