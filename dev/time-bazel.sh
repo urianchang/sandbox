2e790f4d03b7782cf6359eaf86ba43784aac434b
Urian Chang <urian.chang@dominodatalab.com>
2023-04-19 21:14:25 +0000

    Updates after running bazel cache tests

--- dev/time-bazel.sh
+++ dev/time-bazel.sh
@@ -9,7 +9,11 @@ echo "=== Running Bazel build (all) ==="
 bazel build //...
 build_duration=$(($SECONDS - $clean_duration))
 
+echo "=== Running Bazel test (all) ==="
+bazel test //... --nocache_test_results
+test_duration=$(($SECONDS - $build_duration - $clean_duration))
 
 echo "Clean: $clean_duration second(s)"
 echo "Build: $build_duration second(s)"
+echo "Test: $test_duration second(s)"
 echo "Total: $SECONDS second(s)"
